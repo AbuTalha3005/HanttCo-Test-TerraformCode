d62abb9d62d1fa5fbaac13b9ae1a93515df21819
AbuTalha3005 <48378347+AbuTalha3005@users.noreply.github.com>
2025-05-18 21:12:44 +0800

    Update backend.tf

--- Terraform-Code/HanttCo/dev/backend.tf
+++ Terraform-Code/HanttCo/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "sampletfstatebucket"                 # Replace with your S3 bucket name
-    key            = "nginx-windows/dev/terraform.tfstate"         # Unique state file path for dev
+    bucket         = "sampletfstatebucket"
+    key            = "nginx-windows/dev/terraform.tfstate"
     region         = "ap-southeast-1"
     encrypt        = true
     use_lockfile   = true
